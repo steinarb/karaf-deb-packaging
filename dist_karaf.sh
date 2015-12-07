39c9017d07f54f32e33b42d434895f99bb49a7df
rizzotto <demis@exxoss.com>
2015-12-07 09:21:20 +0100

    add symlinc for etc config files

--- dist_karaf.sh
+++ dist_karaf.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 name=karaf
 version=4.0.3
-package_version="-1"
+package_version="-2"
 description="Apache Karaf is a modern and polymorphic container."
 url="https://karaf.apache.org/"
 arch="all"
